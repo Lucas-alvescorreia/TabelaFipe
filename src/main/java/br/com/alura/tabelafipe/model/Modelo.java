c36736adfd1d315e61604ac5e9b841ef15362549
Lucas Alves <lucas.alvessp10@outlook.com>
2024-04-18 02:10:24 -0300

    Revert "Modelagem e tratamento de dados"
    This reverts commit 3d0359f8015b24e34c623f3e6406ec18ced92f62.

--- src/main/java/br/com/alura/tabelafipe/model/Modelo.java
+++
@@ -1,8 +0,0 @@
-package br.com.alura.tabelafipe.model;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import java.util.List;
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record Modelo(List<Dados> modelos) {
-}
