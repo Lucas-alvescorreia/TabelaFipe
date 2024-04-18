d3ff6c32c0427dbd64f1fbdb7ee029595db77d6e
Lucas Alves <lucas.alvessp10@outlook.com>
2024-04-18 02:10:26 -0300

    Revert "Mapeamento de classes"
    This reverts commit dfaf7c1828b6f8ebd8ceb49862f969d315fcef8e.

--- src/main/java/br/com/alura/tabelafipe/model/DadosMarca.java
+++
@@ -1,9 +0,0 @@
-package br.com.alura.tabelafipe.model;
-
-import com.fasterxml.jackson.annotation.JsonAlias;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record DadosMarca(@JsonAlias("codigo") Integer codigo,
-                         @JsonAlias("nome") String nome) {
-}
--- src/main/java/br/com/alura/tabelafipe/service/IConverteDados.java
+++
@@ -1,5 +0,0 @@
-package br.com.alura.tabelafipe.service;
-
-public interface IConverteDados {
-    <T> T obterDados(String Json, Class<T> classe) ;
-}
